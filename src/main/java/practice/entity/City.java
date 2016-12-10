0474a3475ff9bab4078b0b7e60acfdd9ccd96720
Administrator <Administrator@EDW2ZQTUHVBZW3E>
2016-12-10 13:52:01 +0800

    modify ignore

---
+++ src/main/java/practice/entity/City.java
@@ -0,0 +1,44 @@
+package practice.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cities")
+public class City {
+	@Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private int id;
+	private String code;
+	private String name;
+	private String info;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getInfo() {
+		return info;
+	}
+	public void setInfo(String info) {
+		this.info = info;
+	}
+	
+}
