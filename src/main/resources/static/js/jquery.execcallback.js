9d69b07ae7753f8517699afa8bf692570bee71af
Michael Yan <mryanhaoxd@163.com>
2016-12-13 13:16:29 +0800

    fix juqery

--- src/main/resources/static/js/jquery.execcallback.js
+++ src/main/resources/static/js/jquery.execcallback.js
@@ -6,54 +6,51 @@
 	    var hostPath=fullPath.substring(0,pos);
 	    var projectName=pathName.substring(0,pathName.substr(1).indexOf('/')+1)+'/';
 	    return(hostPath+projectName);
-	} 
+	};
 	
 	cmdMap = {
-            LOGIN: 'user/loginPage.html'
-	}
+            LOGIN: 'user/login'
+	};
 	
 	var _ajax = $.ajax;
 	$.ajax = function(opt) {
-		var fn = {
-			error : function(XMLHttpRequest, textStatus, errorThrown) {
-			},
-			success : function(data, textStatus) {
-			}
-		}
-		if (opt.error) {
-			fn.error = opt.error;
-		}
-		if (opt.success) {
-			fn.success = opt.success;
-		}
+            var fn = {
+                error : function(XMLHttpRequest, textStatus, errorThrown) {
+                },
+                success : function(data, textStatus) {
+                }
+            }
+            if (opt.error) {
+                fn.error = opt.error;
+            }
+            if (opt.success) {
+                fn.success = opt.success;
+            }
 
-		var _opt = $.extend(opt, {
-			error : function(XMLHttpRequest, textStatus, errorThrown) {
-				fn.error(XMLHttpRequest, textStatus, errorThrown);
-			},
-			success : function(data, textStatus) {
-				if(data!=null && data!=undefined && data!="") {
-					if(data.cmd) {
-						layer.alert(data.msg, function(index){
-							switch (data.cmd) {
-							case 'CMD_LOGIN':
-								alert('jump~');
-								location.href = ROOTPATH() + cmdMap.LOGIN + "?callback="+data.callback + "&r=" + Math.random();
-								return;
-							default:
-								break;
-							}
-							
-						    layer.close(index);
-						});
-					} else {
-						fn.success(data, textStatus);
-					}
-				} else {
-					layer.alert("no data response.");
-				}
-			}
-		});
-		_ajax(_opt);
+            var _opt = $.extend(opt, {
+                error : function(XMLHttpRequest, textStatus, errorThrown) {
+                    fn.error(XMLHttpRequest, textStatus, errorThrown);
+                },
+                success : function(data, textStatus) {
+                    if(data!==null && data!==undefined && data!=="") {
+                        if(data.cmd) {
+                            //execute command here
+                            switch (data.cmd) {
+                            case 'CMD_LOGIN':
+                                alert('jump~');
+                                location.href = ROOTPATH() + cmdMap.LOGIN + "?callback="+data.callback + "&r=" + Math.random();
+                                return;
+                            default:
+                                break;
+                            }
+                        } else {
+                            fn.success(data, textStatus);
+                        }
+                    } else {
+                        window.alert("no data response.");
+                    }
+                }
+            });
+            _ajax(_opt);
 	};
 })(jQuery);
