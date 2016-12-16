26c2d959c388c0d391225a717916a27f904e2386
Michael Yan <mryanhaoxd@163.com>
2016-12-16 17:19:06 +0800

    Failed repo test case

--- src/main/java/practice/repo/impl/CachedObjectsRespositoryImpl.java
+++ src/main/java/practice/repo/impl/CachedObjectsRespositoryImpl.java
@@ -3,31 +3,50 @@ package practice.repo.impl;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import javax.annotation.Resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
+import practice.entity.City;
 
 import practice.entity.Customer;
 import practice.repo.CachedObjectsRespository;
+import practice.repo.CityRepository;
 import practice.repo.CustomerRepository;
 
-public class CachedObjectsRespositoryImpl implements CachedObjectsRespository{
-	private Object lock;
-	
-	@Autowired
-	private CustomerRepository cp;
-	
-	@Override
-	@Cacheable("customers")
-	public List<Customer> getAllCustomers() {
-		List<Customer> customers = new ArrayList<>();
-		synchronized (lock) {
-			Iterator<Customer> it = cp.findAll().iterator();
-			while(it.hasNext()){
-				customers.add(it.next());
-			}
-		}
-		return customers;
-	}
+public class CachedObjectsRespositoryImpl implements CachedObjectsRespository {
+
+    private final Object lock = new Object();
+
+    @Autowired
+    private CustomerRepository cp;
+    @Resource(name = "cityRepo")
+    private CityRepository cityRepo;
+
+    @Override
+    @Cacheable("customers")
+    public List<Customer> getAllCustomers() {
+        List<Customer> customers = new ArrayList<>();
+        synchronized (lock) {
+            Iterator<Customer> it = cp.findAll().iterator();
+            while (it.hasNext()) {
+                customers.add(it.next());
+            }
+        }
+        return customers;
+    }
+
+    @Override
+    @Cacheable("cities")
+    public List<City> getAllCities() {
+        List<City> cities = new ArrayList<>();
+        synchronized(lock) {
+            Iterator<City> it = cityRepo.findAll().iterator();
+            while(it.hasNext()){
+                cities.add(it.next());
+            }
+        }
+        return cities;
+    }
 
 }
---
+++ src/test/java/practice/repo/RepoBaseTest.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practice.repo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+
+/**
+ *
+ * @author myan
+ */
+@ContextConfiguration(classes = SpringConfiguration.class)
+public class RepoBaseTest {
+    
+}
+@Configuration
+class SpringConfiguration{
+}
