7b44e29c9183ff12d68e3e8b70598993c5d4d794
Hannah Huo <huochenhui1008@163.com>
2017-03-10 22:24:18 +0800

    Basic task module

--- src/main/java/org/jsche/entity/Task.java
+++ src/main/java/org/jsche/entity/Task.java
@@ -5,10 +5,119 @@
  */
 package org.jsche.entity;
 
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+
 /**
  *
  * @author myan
  */
-public class Task {
-    
+@Entity
+@Table(name = "tasks")
+public class Task implements Serializable {
+	private static final long serialVersionUID = -6215223549526696658L;
+
+	public enum TaskType {
+		FAMILY_ISSUE("Family Issue"),
+		SELF_IMPROVEMENT("Self Improvement"), 
+		SOCIAL_ACTIVITY("Social Activity"), 
+		WORK_TASK("Work Task"), 
+		OTHER_ISSUR("Other Issue");
+
+		private String typeName;
+
+		TaskType(String name) {
+			this.typeName = name;
+		}
+
+		public String getTypeName() {
+			return typeName;
+		}
+		
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String taskName;
+	private TaskType taskType;
+	private int status;
+	private int priority;
+	@Temporal(javax.persistence.TemporalType.DATE)
+	private Date startDate;
+	@Temporal(javax.persistence.TemporalType.DATE)
+	private Date endDate;
+	private int userId;
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTaskName() {
+		return taskName;
+	}
+
+	public void setTaskName(String taskName) {
+		this.taskName = taskName;
+	}
+
+	public TaskType getTaskType() {
+		return taskType;
+	}
+
+	public void setTaskType(TaskType taskType) {
+		this.taskType = taskType;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+	public int getPriority() {
+		return priority;
+	}
+
+	public void setPriority(int priority) {
+		this.priority = priority;
+	}
+
 }
---
+++ src/main/java/org/jsche/service/impl/TaskServiceImpl.java
@@ -0,0 +1,27 @@
+package org.jsche.service.impl;
+
+import java.util.List;
+
+import org.jsche.entity.Task;
+import org.jsche.entity.Task.TaskType;
+import org.jsche.repo.TaskRepository;
+import org.jsche.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("taskService")
+public class TaskServiceImpl implements TaskService{
+	@Autowired
+	private TaskRepository tp;
+	
+	@Override
+	public TaskType[] buildTypeArray() {
+		return TaskType.values();
+	}
+
+	@Override
+	public List<Task> getUserTasks(int userId) {
+		return tp.getUserTasks(userId);
+	}
+
+}
