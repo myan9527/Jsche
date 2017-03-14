987423194431fb03a3c6ea404442834eef03faa3
myan <mike.yan@activenetwork.com>
2017-03-14 16:14:41 +0800

    control avatar size.

--- src/main/java/org/jsche/common/util/AppUtil.java
+++ src/main/java/org/jsche/common/util/AppUtil.java
@@ -7,64 +7,66 @@ import java.util.Date;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.jsche.common.Constants;
+
 public final class AppUtil {
-	private final static SimpleDateFormat sdf = new SimpleDateFormat("yy-MM-dd HH:mm:ss");
+    private final static SimpleDateFormat sdf = new SimpleDateFormat("yy-MM-dd HH:mm:ss");
 
-	public static String getHexPassword(String src) {
-		if (src == null || src.trim().length() == 0) {
-			return null;
-		}
-		char hexDigits[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
-		try {
-			byte[] btInput = src.getBytes();
-			MessageDigest mdInst = MessageDigest.getInstance("MD5");
-			mdInst.update(btInput);
-			byte[] md = mdInst.digest();
-			int j = md.length;
-			char str[] = new char[j * 2];
-			int k = 0;
-			for (int i = 0; i < j; i++) {
-				byte byte0 = md[i];
-				str[k++] = hexDigits[byte0 >>> 4 & 0xf];
-				str[k++] = hexDigits[byte0 & 0xf];
-			}
-			return new String(str);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
+    public static String getHexPassword(String src) {
+        if (src == null || src.trim().length() == 0) {
+            return null;
+        }
+        char hexDigits[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+        try {
+            byte[] btInput = src.getBytes();
+            MessageDigest mdInst = MessageDigest.getInstance("MD5");
+            mdInst.update(btInput);
+            byte[] md = mdInst.digest();
+            int j = md.length;
+            char str[] = new char[j * 2];
+            int k = 0;
+            for (int i = 0; i < j; i++) {
+                byte byte0 = md[i];
+                str[k++] = hexDigits[byte0 >>> 4 & 0xf];
+                str[k++] = hexDigits[byte0 & 0xf];
+            }
+            return new String(str);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
-	public static boolean validHexPassword(String src) {
-		return false;
-	}
+    public static boolean validHexPassword(String src) {
+        return false;
+    }
 
-	public static String formatDate(Date dateStamp) {
-		return sdf.format(dateStamp);
-	}
+    public static String formatDate(Date dateStamp) {
+        return sdf.format(dateStamp);
+    }
 
-	public static String getClienIp(HttpServletRequest request) {
-		String ip = request.getHeader("x-forwarded-for");
-		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-			ip = request.getHeader("Proxy-Client-IP");
-		}
-		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-			ip = request.getHeader("WL-Proxy-Client-IP");
-		}
-		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-			ip = request.getHeader("HTTP_CLIENT_IP");
-		}
-		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-			ip = request.getHeader("HTTP_X_FORWARDED_FOR");
-		}
-		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-			ip = request.getRemoteAddr();
-		}
-		return ip;
-	}
-	
-	public static String generateAvatar(String src){
-	    try {
+    public static String getClienIp(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
+    }
+
+    public static String generateAvatar(String src) {
+        try {
             MessageDigest md = MessageDigest.getInstance("MD5");
             md.update(src.getBytes());
             byte[] bytes = md.digest();
@@ -72,10 +74,10 @@ public final class AppUtil {
             for (int i = 0; i < bytes.length; i++) {
                 buffer.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
             }
-            return "URL: http://www.gravatar.com/avatar/" + buffer.toString();
+            return "URL: http://www.gravatar.com/avatar/" + buffer.toString() + "?size=" + Constants.AVATAR_SIZE;
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
             return null;
         }
-	}
+    }
 }
