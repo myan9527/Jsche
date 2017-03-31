e8668e8b746cb55acb34c8fda667605fd7cac531
myan <mike.yan@activenetwork.com>
2017-03-31 11:43:51 +0800

    config for undertow.

--- src/main/java/org/jsche/config/ServerConfig.java
+++ src/main/java/org/jsche/config/ServerConfig.java
@@ -1,8 +1,12 @@
 package org.jsche.config;
 
+import io.undertow.UndertowOptions;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.undertow.UndertowEmbeddedServletContainerFactory;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.ErrorPage;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 
@@ -21,8 +25,17 @@ public class ServerConfig implements EmbeddedServletContainerCustomizer {
         ErrorPage error500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/500.html");
         container.setSessionTimeout(45, TimeUnit.MINUTES);
         container.addErrorPages(error404, error403, error500);
-        container.setPort(8000);
-
     }
 
+    @Bean
+    @ConfigurationProperties(prefix = "server.undertow")
+    public UndertowEmbeddedServletContainerFactory embeddedServletContainerFactory() {
+        UndertowEmbeddedServletContainerFactory undertow = new UndertowEmbeddedServletContainerFactory();
+        undertow.addBuilderCustomizers(builder -> {
+            builder.setServerOption(UndertowOptions.ENABLE_HTTP2, true);
+//            builder.addHttpsListener(8443,"local")
+        });
+
+        return undertow;
+    }
 }
