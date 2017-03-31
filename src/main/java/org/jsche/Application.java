a79b503c03aa7f90794a018f5109dc631035de14
myan <mike.yan@activenetwork.com>
2017-03-31 14:50:57 +0800

    Dashboard page.

--- src/main/java/org/jsche/Application.java
+++ src/main/java/org/jsche/Application.java
@@ -2,23 +2,12 @@ package org.jsche;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.boot.web.servlet.ErrorPage;
-import org.springframework.http.HttpStatus;
-
-import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
-public class Application implements EmbeddedServletContainerCustomizer {
+public class Application {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class);
-    }
-
-    @Override
-    public void customize(ConfigurableEmbeddedServletContainer container) {
-
     }
 
 }
