00be22916b5678be062be1ac6778ef28e33c90ca
myan <mike.yan@activenetwork.com>
2017-04-18 14:21:09 +0800

    Add druid watch.

--- src/main/java/org/jsche/config/DataSourceConfig.java
+++ src/main/java/org/jsche/config/DataSourceConfig.java
@@ -10,7 +10,6 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 
@@ -20,20 +19,19 @@ import java.util.Map;
 @Configuration
 @MapperScan("org.jsche.web.dao")
 public class DataSourceConfig {
-	
-	@ConfigurationProperties(prefix = "datasource.druid")
-	@Bean(name="datasource",initMethod = "init",destroyMethod = "close")
-    public DruidDataSource getDataSource(){
-         return new DruidDataSource();
+
+    @ConfigurationProperties(prefix = "spring.datasource")
+    @Bean
+    public DruidDataSource getDataSource() {
+        return new DruidDataSource();
     }
-	
-	@Bean
-    public PlatformTransactionManager transactionManager(){
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
         return new DataSourceTransactionManager(getDataSource());
     }
-	
-	@Bean
-    @Profile("dev")
+
+    @Bean
     public FilterRegistrationBean druidFilter() {
         FilterRegistrationBean registration = new FilterRegistrationBean();
         registration.setFilter(new WebStatFilter());
@@ -43,10 +41,9 @@ public class DataSourceConfig {
         registration.setUrlPatterns(Lists.newArrayList("/*"));
         registration.setInitParameters(intParams);
         return registration;
-    }	
-	
-	@Bean
-    @Profile("dev")
+    }
+
+    @Bean
     public ServletRegistrationBean servletRegistrationBean() {
         ServletRegistrationBean registration = new ServletRegistrationBean();
         registration.setServlet(new StatViewServlet());
@@ -54,8 +51,8 @@ public class DataSourceConfig {
         registration.setUrlMappings(Lists.newArrayList("/druid/*"));
         //自定义添加初始化参数
         Map<String, String> intParams = new HashMap<>();
-        intParams.put("loginUsername","druid");
-        intParams.put("loginPassword","druid");
+        intParams.put("loginUsername", "druid");
+        intParams.put("loginPassword", "druid");
         registration.setName("DruidWebStatFilter");
         registration.setInitParameters(intParams);
         return registration;
